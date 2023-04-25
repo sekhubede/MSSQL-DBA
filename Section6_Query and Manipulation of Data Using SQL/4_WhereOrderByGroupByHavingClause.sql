17feddad501e98b8b6217b6e208061eeb0088eac
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-25 17:01:30 +0200

    Added theory and syntax.

---
+++ Section6_Query and Manipulation of Data Using SQL/4_WhereOrderByGroupByHavingClause.sql
@@ -0,0 +1,47 @@
+/*
+Theory:
+
+-- WHERE Clause
+- Used to specify a condition while fetching the data from a single table or by joining with multiple tables.
+- To filter the records and fetching only the necessary records.
+- Not only used in the ELECT statement, but it is also used in the UPDATE, DELETE statement, etc.
+
+*/
+-- Syntax
+SELECT [column1], [column2], [columnN]
+FROM [table_name]
+WHERE [condition]
+
+-- Practical
+
+/*
+Theory:
+
+ORDER BY Clause
+- Used to sort the data in ascending or descending order, based on one or more columns.
+- Some databases sort the query results in an ascending order by default.
+
+*/
+-- Syntax
+SELECT [column-list]
+FROM [table_name]
+WHERE [condition]
+ORDER BY [column1], [column2], [columnN] [ASC | DESC]
+
+-- Practical
+
+/*
+Theory:
+
+*/
+-- Syntax
+
+-- Practical
+
+/*
+Theory:
+
+*/ 
+-- Syntax
+
+-- Practical
