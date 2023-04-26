37a3e7c29356d3e021223311d2a0da67e2578588
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-26 16:44:25 +0200

    Added to the theory and syntax.

--- Section6_Query and Manipulation of Data Using SQL/4_WhereOrderByGroupByHavingClause.sql
+++ Section6_Query and Manipulation of Data Using SQL/4_WhereOrderByGroupByHavingClause.sql
@@ -33,15 +33,38 @@ ORDER BY [column1], [column2], [columnN] [ASC | DESC]
 /*
 Theory:
 
+GROUP BY Clause
+- Used in collaboration with the SELECT statement to arrange identical data into groups.
+- like "find the number of customers in each country"
+- Often used with aggregate functions (COUNT(), MAX(), MIN(), SUM(), AVG()) to group the result-set by one or more columns.
+- Follows the WHERE clause in a SELECR statement and precedes the ORDER BY clause.
+
 */
 -- Syntax
+SELECT [column1], [column2]
+FROM [table_name]
+WHERE [conditions]
+GROUP BY [column1], [column2]
+ORDER BY [column1], [column2]
 
 -- Practical
 
 /*
 Theory:
 
+HAVING Clause
+- Enables you to specify conditions that filter which group results appear in the results.
+- The WHERE clause places conditions on the selected columns.
+- Whereas the HAVING clause places conditions on groups created by the GROUP BY clause.
+- HAVING clause must follow the FROUP BY clause in a query and must also precede the ORDER BY clause if used.
+
 */ 
 -- Syntax
+SELECT [column1], [column2]
+FROM [table_name]
+WHERE [conditions]
+GROUP BY [column1], [column2]
+HAVING [conditions]
+ORDER BY [column1], [column2]
 
 -- Practical
