df822166c9c40c9e188040e11def258b11ab1bd8
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-06-07 16:04:15 +0200

    Finalized the notes for the intro to joins

--- Section6_Query and Manipulation of Data Using SQL/5_Joins.sql
+++ Section6_Query and Manipulation of Data Using SQL/5_Joins.sql
@@ -21,7 +21,61 @@
 -- End: Joins
 
 -- Start: Types of Joins
- --
+
+ -- Start: INNER Join
+  -- Returns common rows from both tables as long as the condition is satisfied.
+
+  -- Syntax
+  SELECT [table1_column], [table1_column], [table2_column]
+  FROM [table1_name]
+  INNER JOIN [table2_name]
+  ON [table1_matchingcolumn] = [table2_matchingcolumn]
+
+  -- Practical
+
+ -- End: INNER Join
+
+ -- Start: LEFT Join
+  -- Return all rows from left table and matching rows from the right table
+  
+  -- Syntax
+  SELECT [table1_column], [table1_column], [table2_column]
+  FROM [table1_name]
+  LEFT JOIN [table2_name]
+  ON [table1_matchingcolumn] = [table2_matchingcolumn]
+
+  -- Practical
+
+ -- End: LEFT Join
+
+ -- Start: RIGHT Join
+  -- Return all rows from right table and matching rows from the left table
+  
+  -- Syntax
+  SELECT [table1_column], [table1_column], [table2_column]
+  FROM [table1_name]
+  RIGHT JOIN [table2_name]
+  ON [table1_matchingcolumn] = [table2_matchingcolumn]
+
+  -- Practical
+
+ -- End: RIGHT Join
+
+ -- Start: FULL Join
+  -- Return all rows from both tables
+
+  -- Syntax
+  SELECT [table1_column], [table1_column], [table2_column]
+  FROM [table1_name]
+  FULL JOIN [table2_name]
+  ON [table1_matchingcolumn] = [table2_matchingcolumn]
+
+
+  -- Practical
+
+
+ -- End: FULL Join
+
 -- End: Types of Joins
 
 
