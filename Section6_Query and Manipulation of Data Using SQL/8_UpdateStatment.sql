25a6293b0233d29d6eb57a41bf0bc9708e4dc318
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-07-18 16:57:54 +0200

    Add syntax and practical notes

--- Section6_Query and Manipulation of Data Using SQL/8_UpdateStatment.sql
+++ Section6_Query and Manipulation of Data Using SQL/8_UpdateStatment.sql
@@ -1,8 +1,27 @@
--- Start: Insert Statement
+-- #region Update Statement
 
     -- Syntax
-
+    UPDATE [table1_name]
+    SET [column1] = [value1],
+    [column2] = [value2],
+    [columnN] = [valueN]
+    WHERE [columnN]
 
     -- Practical
+    UPDATE [staffSales] SET [SQuota] = 500000 -- all rows will be updated
 
--- End: Inser Statment
+    UPDATE [staffSales] SET [SQuota] = [SQuota] + 50000
+
+    UPDATE [staffSales] SET [SQuota] = [SQuota] + 50000, 
+    [SYTD] = 0, [SLastYear] = [SLastYear] * 1.05 -- update multiple columns
+
+    UPDATE [staffSales] SET [TerritoryName] = 'UK'
+    WHERE [TerritoryName] = 'United Kingdom' -- filter records to update
+
+    UPDATE [staffSales] SET [SQuota] = [sp.SQuota]
+    FROM [SalesStaff] AS [ss]
+    INNER JOIN [Sales.vSalesPerson] AS [sp]
+    ON [ss.FullName] = ([sp.FirstName] + ' ' + [sp.LastName]) 
+    -- update records based on another tables data
+
+-- #endregiond
