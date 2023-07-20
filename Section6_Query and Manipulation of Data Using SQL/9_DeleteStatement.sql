28453585794bc71a1dfff81dfed8c344ebef9c75
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-07-20 14:41:32 +0200

    Add tansaction deletion notes

--- Section6_Query and Manipulation of Data Using SQL/9_DeleteStatement.sql
+++ Section6_Query and Manipulation of Data Using SQL/9_DeleteStatement.sql
@@ -27,5 +27,17 @@
     [sp.BusinessEntityID] = [ss.StaffID]
     WHER [sp.SalesLastYear] = 0;
 
+    -- In production environment use a transaction to delete
+    -- deletes the records from the table but does not commit it to memory
+    BEGIN TRAN
+    DELETE FROM [SalesStaff] 
+    WHERE [CountyRegion] = 'united states'
+    -- meaning if something was wrong with the query above
+    -- we can do the following
+    ROLLBACk TRAN
+    -- rolls back the deleted records
+
+    -- if you are happy with the query you can run the commit without the rollback tran
+    COMMIT -- to complete the delete when using the begin tran keyword
 
 -- #endregion
