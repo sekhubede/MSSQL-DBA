a3696f131f2a716e563dcfd4d8d1750507255adf
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-07-20 12:05:19 +0200

    Add delete statment notes

--- Section6_Query and Manipulation of Data Using SQL/9_DeleteStatement.sql
+++ Section6_Query and Manipulation of Data Using SQL/9_DeleteStatement.sql
@@ -1,8 +1,31 @@
--- Start: Insert Statement
+-- #region Delete Statement
 
     -- Syntax
+    DELETE [table_name]
 
+    DELETE FROM [table_name] -- 'FROM' keyword is optional
+
+    DELETE TOP (20) {PERCENT} FROM [SalesStaff]
+
+    DELETE 
 
     -- Practical
+    DELETE FROM [SalesStaff] -- 'FROM' keyword is optional
+    WHERE [CountyRegion] = 'United States'
 
--- End: Inser Statment
+    DELETE [SalesStaff]
+    WHERE [StaffID] IN 
+    (
+        SELECT [BusinessEntityID] 
+        FROM [Sales.vSalesPerson]
+        WHERE [SalesLastYear = 0]
+    )
+
+    DELETE [SalesStaff]
+    FROM [Sales.vSalesPerson] AS [sp]
+    INNER JOIN [dbo].[SalesStaff] AS [ss] ON
+    [sp.BusinessEntityID] = [ss.StaffID]
+    WHER [sp.SalesLastYear] = 0;
+
+
+-- #endregion
